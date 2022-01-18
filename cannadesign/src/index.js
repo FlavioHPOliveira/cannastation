f6f05a8c270911d8aca2f1e15dfdc8d56c1a218e
FlavioHPOliveira <flaviohenri7@hotmail.com>
2022-01-18 19:26:35 -0300

    added login form

--- cannadesign/src/index.js
+++ cannadesign/src/index.js
@@ -1,17 +1,27 @@
 // Import the functions you need from the SDKs you need
+import { async } from "@firebase/util";
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getAuth, connectAuthEmulator, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
 //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
 //import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-auth.js";
-
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// import {
+//   hideLoginError,
+//   showLoginState,
+//   showLoginForm,
+//   showApp,
+//   showLoginError,
+//   btnLogin,
+//   btnSignup,
+
+// }
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD1P_W4y62l8-OESX1jdW-ffuz4ub3KE2M",
+  apiKey: "AIzaSyD1P_W4y62l8-OESX1jdW-ffuz4u  b3KE2M",
   authDomain: "cannafirebase.firebaseapp.com",
   projectId: "cannafirebase",
   storageBucket: "cannafirebase.appspot.com",
@@ -19,15 +29,23 @@ const firebaseConfig = {
   appId: "1:262309337307:web:2e269d47d5485d0d6314fe"
 };
 
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp)
 
-const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
 
-onAuthStateChanged(auth, user =>{
-  if(user != null){
-    console.log('logged in!')
-  }else{
-    console.log('no user')
-  }
-})
+const btnLog = document.querySelector("#btnLogin")
+console.log(btnLog)
+
+const loginEmailPassword = async () => {
+  const loginEmail = document.querySelector("#txtEmail").value;
+  const loginPwd = document.querySelector("#txtPwd").value;
+
+  const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPwd)
+  console.log(userCredential.user)
+
+}
+
+btnLog.addEventListener('click', loginEmailPassword)
