9eb70ed61b9957df9de9b807113a455d0c270e19
FlavioHPOliveira <flaviohenri7@hotmail.com>
2022-02-08 14:58:59 -0300

    design changes

--- cannadesign/webpack.config.js
+++ cannadesign/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: {
     login: './src/js/login.js',
     station: ['./src/js/station.js', './src/js/modal.js'],
-    product: './src/js/product.js'
+    product: './src/js/product.js',
+    index: './src/js/index.js'
   },
   // The location of the build folder described above
   output: {
